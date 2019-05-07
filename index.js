7886840ec283c6a973c4b16b922c36784deee0bc
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-05-07 15:20:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here
+
+let var1 = 62
+let var2 = 1
+let multiply = var1 * var2
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 40 % 9
+
+let max = Math.max(1,20)
